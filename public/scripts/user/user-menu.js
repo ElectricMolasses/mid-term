8050affe24b25ead296640106cdb39959d7cbe28
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-11-15 12:58:38 +0000

    Get all menu data to html

--- public/scripts/user/user-menu.js
+++ public/scripts/user/user-menu.js
@@ -23,26 +23,21 @@ $(() => {
   `;
 
 //get data from GET /menu
-//loop through data and render to span??
+//loop through data and render to table
 
   $(".user-order-now").on('click', (() => {
     event.preventDefault();
-    
-    
     $.ajax("user/menu", {
       method: 'GET',
       dataType: "json",
     }).done((data) => {
-      
       $.each(data, (index) => {
         const $temp = $(template);
-        console.log('test1', index, data[index].name);
         $temp.find(".user-item-name").text(data[index].name);
         $temp.find(".user-item-description").text(data[index].description);
         $temp.find(".user-item-price").text(data[index].cost);
         $(".user-menu").append($temp);
       })
-      
     })
   }))
 
