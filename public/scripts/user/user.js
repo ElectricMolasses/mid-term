7f1ac58823beb2142d71a9d75219aa661590dded
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-11-15 06:03:43 +0000

    Fix lint in user.js

--- public/scripts/user/user.js
+++ public/scripts/user/user.js
@@ -1,38 +1,40 @@
-  //get login/register form popup
-  $(() => {
-    $(".user-login").on('click',(() => {
-      if ($(".user-login-form").hasClass('visible')) {
-        $(".user-login-form").hide().removeClass('visible');
-      } else {
-        $(".user-login-form").show().addClass('visible');
-      }
-    })
-    );
-  });
+//get login/register form popup
+$(() => {
+  $(".user-login").on('click',(() => {
+    if ($(".user-login-form").hasClass('visible')) {
+      $(".user-login-form").hide().removeClass('visible');
+    } else {
+      $(".user-login-form").show().addClass('visible');
+    }
+  })
+  );
+});
 
-  $(() => {
-    $(".user-signup").on('click',(() => {
-      if ($(".user-signup-form").hasClass('visible')) {
-        $(".user-signup-form").hide().removeClass('visible');
-      } else {
-        $(".user-signup-form").show().addClass('visible');
-      }
-    })
-    );
-  });
+$(() => {
+  $(".user-signup").on('click',(() => {
+    if ($(".user-signup-form").hasClass('visible')) {
+      $(".user-signup-form").hide().removeClass('visible');
+    } else {
+      $(".user-signup-form").show().addClass('visible');
+    }
+  })
+  );
+});
+
+/*POST signin data.
+Login button changed to logged
+Hide Register button
+Show shopping cart and total amount
+*/
+
+$(() => {
+
+});
+//scroll down/unhide to nav and full menu
 
 
-  /*POST signin data. 
-  Login button changed to logged
-  Hide Register button
-  Show shopping cart and total amount
-  */
-
-  //scroll down/unhide to nav and full menu 
+//user-nav-menu - click categories to scroll to specific table in full menu
 
 
-  //user-nav-menu - click categories to scroll to specific table in full menu
-
-
-  //user-
+//user-
 
