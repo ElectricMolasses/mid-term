d159b5033a1f00ec04f2877500dab544fcc667a2
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-11-15 09:55:03 +0000

    Login feature works

--- public/scripts/user/user.js
+++ public/scripts/user/user.js
@@ -11,7 +11,6 @@ $(() => {
   })
   );
 
-
   $(".user-signup").on('click',(() => {
     if ($(".user-signup-form").hasClass('visible')) {
       $(".user-signup-form").hide().removeClass('visible');
@@ -21,13 +20,12 @@ $(() => {
   })
   );
 
-
-/*POST signin data.
-Login button changed to logged
-Hide Register button
-Show shopping cart and total amount
-logout button
-*/
+  /*POST signin data.
+  Login button changed to logged
+  Hide Register button
+  Show shopping cart and total amount
+  logout button
+  */
 
   let $button = $(".user-login-button");
   $button.on('click', (() => {
@@ -37,10 +35,9 @@ logout button
       method: 'POST',
       dataType: "json",
       data: {
-        email: $(".user-email").val(), //need store email in database
+        email: $(".user-email").val(),
         password: $(".user-password").val()
       }
-
     })
       .done((res) => {
         console.log('1',res);
@@ -51,44 +48,21 @@ logout button
         } else {
           console.log('here');
           $(".user-login-form").hide().removeClass('visible');
-          $(".user-access").hide().removeClass('visible');
-          $(".user-logged").show().addClass('visible');
           // need add scroll down to menu section
         }
-    })
-
-
-    // $.get('/login')
-
+      });
     $.ajax('/user/profile', {
       method: 'GET',
-      dataType: "json",
-
-    })
+      dataType: "json"
+    }).done((data) => {
+      console.log(data.first_name);
+      $(".user-access").hide().removeClass('visible');
+      $(".user-email1").text(data.first_name);
+      $(".user-logged").show().addClass('visible');
+    });
   }));
-  //need to check data with our database
-
-    //false - pop up message
-//   if (2) {
-//     console.log('error')
-//   } else {   // true
-//     $("user-login-form").hide().removeClass('visible');
-//     $("user-login").hide().removeClass('visible');
-//     $("user-signup").hide().removeClass('visible');
-//     //unhide logged info and shopping cart
-//   }
-//  })
-//  );
-
-
 });
 
 
-//scroll down/unhide to nav and full menu
 
-
-//user-nav-menu - click categories to scroll to specific table in full menu
-
-
-//user-
 
