3d2580fcf2117e16b519f044687a00da34695a62
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-11-15 11:35:24 +0000

    create template for user-menu

--- public/scripts/user/user-menu.js
+++ public/scripts/user/user-menu.js
@@ -1,4 +1,20 @@
 //menu display - pull data from database
+<template class="user-menu">
+  <table>
+    <tr>
+      <td>
+        <a>name</a>
+        <a>description</a>
+      </td>
+      <td>price</td>
+      <td>
+        <input class="user-item-add" type="submit" scr="/resources/plus-circle.png"></input>
+      </td>
+    </tr>
+  </table>
+</template>
+
+//-------------------------------------------------
 $(() => {
 
 //get data from GET /menu
@@ -12,7 +28,8 @@ $(() => {
     }).done((data) => {
       $.each(data, (index) => {
         console.log('test1', data[index].name);
-        $(".user-menu").append("<div>", data[index].name, data[index].cost / 100, "</div>");
+        $(".user-menu").append(
+        );
       })
     })
   }))
