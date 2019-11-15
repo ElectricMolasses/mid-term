197a59a201ea5b22658e8e64981da9eb3b7a1db5
Sherry Nguyen <nganguyen.sfu@gmail.com>
2019-11-15 12:43:20 +0000

    Get all menu data to html

--- public/scripts/user/user-menu.js
+++ public/scripts/user/user-menu.js
@@ -1,36 +1,48 @@
 //menu display - pull data from database
-<template class="user-menu">
-  <table>
-    <tr>
-      <td>
-        <a>name</a>
-        <a>description</a>
-      </td>
-      <td>price</td>
-      <td>
-        <input class="user-item-add" type="submit" scr="/resources/plus-circle.png"></input>
-      </td>
-    </tr>
-  </table>
-</template>
+
+
+
 
 //-------------------------------------------------
 $(() => {
 
+  //template for menu
+  const template = `
+  <table class="user-menu-table">
+    <tr>
+      <td>
+        <a class="user-item-name">name</a>
+        <a class="user-item-description">description</a>
+      </td>
+      <td class="user-item-price">$</td>
+      <td>
+        <input class="user-item-add" type="submit"><img scr="/resources/plus-circle.png"></input>
+      </td>
+    </tr>
+  </table>
+  `;
+
 //get data from GET /menu
 //loop through data and render to span??
+
   $(".user-order-now").on('click', (() => {
     event.preventDefault();
+    
     
     $.ajax("user/menu", {
       method: 'GET',
       dataType: "json",
     }).done((data) => {
+      
       $.each(data, (index) => {
-        console.log('test1', data[index].name);
-        $(".user-menu").append(
-        );
+        const $temp = $(template);
+        console.log('test1', index, data[index].name);
+        $temp.find(".user-item-name").text(data[index].name);
+        $temp.find(".user-item-description").text(data[index].description);
+        $temp.find(".user-item-price").text(data[index].cost);
+        $(".user-menu").append($temp);
       })
+      
     })
   }))
 
