ce699cda4470fd3d1c1dac483bbdc2d98a1ded8b
Robert Beckett <robert.beckett@onion.red>
2019-11-16 22:18:49 +0000

    Created create order query for testing.

---
+++ db/queries/52_create_order.sql
@@ -0,0 +1,21 @@
+INSERT INTO orders (
+  customer_id,
+  restaurant_id,
+  time_placed
+) VALUES (
+  1,
+  1,
+  NOW()
+);
+
+INSERT INTO order_items (
+  order_id, item_id
+) VALUES (
+  2, 42
+), (
+  2, 20
+), (
+  2, 16
+), (
+  2, 31
+);
