720772e635194d55f6985aba2704941fc75a83d2
Robert Beckett <robert.beckett@onion.red>
2019-11-16 20:02:50 +0000

    Commented out test code placed into retaurant.js

--- public/scripts/restaurant/restaurant.js
+++ public/scripts/restaurant/restaurant.js
@@ -1,14 +1,14 @@
 $("document").ready(function(){
 $(".restaurant-login-form").hide();
 
-$.ajax('/restaurant/orders/', {
-  method: 'GET'
-})
-  .done((data, status, xhr) => {
-    console.log(data);
-  }).catch(() => {
-    console.log('failed');
-  });
+// $.ajax('/restaurant/orders/', {
+//   method: 'GET'
+// })
+//   .done((data, status, xhr) => {
+//     console.log(data);
+//   }).catch(() => {
+//     console.log('failed');
+//   });
 
 //Click log in button to dsiplay form
 $(".restaurant-login-button").on("click", function() {
