db60f88d4290edf54ae1c1b60d10ec081a247287
Ryan <cordingley.ryan@gmail.com>
2019-11-16 03:35:07 +0000

    added login form to show onclick

---
+++ public/scripts/restaurant/restaurant.js
@@ -0,0 +1,11 @@
+$("document").ready(function(){
+$(".restaurant-login-form").hide();
+//Click log in button to dsiplay form
+$(".restaurant-login-button").on("click", function() {
+  $(".restaurant-login-form").show(500);
+})
+
+
+});
+
+
