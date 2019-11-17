ef5850b08f042d016b4b5bb4df3aa0cb111c30da
Robert Beckett <robert.beckett@onion.red>
2019-11-17 01:03:57 +0000

    Corrected bad undo in users schema/seed.

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -5,6 +5,7 @@ CREATE TABLE users (
   userToken VARCHAR(255),
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   phone_number VARCHAR(16) NOT NULL
 );
