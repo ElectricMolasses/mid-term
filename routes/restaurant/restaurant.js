c13dc36de5be7ff63d772612b66137f151cf65a2
Robert Beckett <robert.beckett@onion.red>
2019-11-18 01:25:28 +0000

    Implemented text message for when a restaurant changes an order time.

--- routes/restaurant/restaurant.js
+++ routes/restaurant/restaurant.js
@@ -113,6 +113,15 @@ module.exports = (db, twilio) => {
         `, [req.params.id, req.body.estimate])
           .then(() => {
             res.json({ status: 'success' });
+            twilio.messages.create({
+              body: `The restaurant has changed the estimated time of completion on your order.
+                    The new estimate time is TEMPORARY TEXT`,
+              to: `+19023945393`,
+              from: `+12029029010`
+            })
+              .then((mes) => {
+                console.log(mes.sid);
+              });
           });
         break;
       case 'complete':
@@ -127,6 +136,14 @@ module.exports = (db, twilio) => {
       }
     }
   });
+  // twilio.messages.create({
+  //   body: 'test message',
+  //   to: '+19023945393',
+  //   from: '+12029029010'
+  // })
+  //   .then((mes) => {
+  //     console.log(mes.sid);
+  //   });
 
   router.get("/update", (req, res) => {
     // Needs to be notified when a user makes an order to this database.  Going to build the users order query first, then work on this.
