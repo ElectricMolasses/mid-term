a16f24befe5cecce89a5af7a05e0e1ee548a54dd
Robert Beckett <robert.beckett@onion.red>
2019-11-18 02:28:01 +0000

    Seeded new order and removed not null from all timestamps aside from placed.

--- db/seeds/07_orders.sql
+++ db/seeds/07_orders.sql
@@ -8,3 +8,11 @@ INSERT INTO orders (
   1, 1, '1999-01-08 04:05:06', '1999-01-08 04:05:06', '1999-01-08 04:05:06'
 );
 
+INSERT INTO orders (
+  customer_id,
+  restaurant_id,
+  time_placed
+) VALUES (
+  1, 1, NOW()
+);
+
