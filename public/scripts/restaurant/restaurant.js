f039197cc04b2ff87d1e24279dadbf06c88ab374
Ryan <cordingley.ryan@gmail.com>
2019-11-19 00:15:53 +0000

    added randomId to div in genreate function

--- public/scripts/restaurant/restaurant.js
+++ public/scripts/restaurant/restaurant.js
@@ -36,7 +36,6 @@ function generateLi(orderItemsObject) {
   let itemHTML = ``;
   for (const i of orderItemsObject) {
     itemHTML += `<li>${i.name}</li> `
-    console.log(i.name);
   }
 return itemHTML;
 }
@@ -45,7 +44,7 @@ function createOrder(i) {
   let timeStamp = i.time_placed.slice(0, 19);
   let div = document.createElement('div');
   div.setAttribute('draggable', 'true');
-  div.setAttribute('id', "hi")
+  div.setAttribute('id', `${randomId()}`)
   div.setAttribute('class', 'restaurant-fill');
   div.innerHTML = (`<div class="restaurant-name-display">
   <p>Name</p>
@@ -69,10 +68,17 @@ function createOrder(i) {
 <p class="restaurant-current-time-elasped">Time Elapsed</p>
 <span class="restaurant-current-time">${(moment(timeStamp).fromNow())}<span>
 </div>`);
-  div.addEventListener("click", function() {
-    div.style.background = "black";
-  })
-  console.log(div);
+
+div.addEventListener('dragstart', () => {
+  this.className += ' hold';
+  setTimeout(() => {
+    this.className = 'invisible'
+  }, 0);
+});
+
+div.addEventListener('dragend', () => {
+  this.className = 'restaurant-empty';
+});
   return div;
 }
 
@@ -81,66 +87,54 @@ $("document").ready(function() {
   $.ajax('/restaurant/orders/', {
     method: 'GET'
   })
-    .done((data, status, xhr) => {
-      console.log(randomId());
-$(".restaurant-login-form").hide();
-//Click log in button to dsiplay form
-$(".restaurant-login-button").on("click", function() {
-    $(".restaurant-login-form").slideToggle("slow", function() {
+  .done((data, status, xhr) => {
+  $(".restaurant-login-form").hide();
+  //Click log in button to dsiplay form
+  $(".restaurant-login-button").on("click", function() {
+  $(".restaurant-login-form").slideToggle("slow", function() {
       //animation complete;
-    });
+  });
   })
 
-// drag and drop
-const empties = document.querySelectorAll(".restaurant-empty");
-// const fill = document.querySelector(".restaurant-fill");
-//loop through empties;
-for (const empty of empties) {
-  empty.addEventListener('dragover', dragOver);
-  empty.addEventListener('dragenter', dragEnter);
-  empty.addEventListener('dragleave', dragLeave);
-  empty.addEventListener('drop', dragDrop);
-}
+    const empties = document.querySelectorAll(".restaurant-empty");
+    for (const empty of empties) {
+      empty.addEventListener('dragover', dragOver);
+      empty.addEventListener('dragenter', dragEnter);
+      empty.addEventListener('dragleave', dragLeave);
+      empty.addEventListener('drop', dragDrop);
+    }
 
+    function dragOver(e) {
+      e.preventDefault();
+    }
 
-//drag function
-function dragStart() {
-  this.className += ' hold';
-  setTimeout(() => {
-    this.className = 'invisible'
-  }, 0);
-}
+    function dragEnter(e) {
+      e.preventDefault();
+      this.className += ' hovered';
+      }
 
-function dragEnd() {
-  this.className = "hold2"
-}
+    function dragLeave() {
+      this.className = 'restaurant-empty';
+    }
 
-function dragOver(e) {
-  e.preventDefault();
-}
+    function dragDrop() {
+      if (this === document.getElementById("restaurant-incoming")) {
+          console.log("drop-1");
+      } else if (this === document.getElementById("restaurant-in-progress")) {
+        console.log("drop-2");
+      } else if (this === document.getElementById("restaurant-complete")) {
+        console.log("drop 3");
+        $(".restaurant-time-started").text(moment());
+        $(".restaurant-time-status").text("Time Complete");
+      }
+      this.className = "restaurant-empty";
+      this.append($('restaurant-fill'));
 
-function dragEnter(e) {
-  e.preventDefault();
-  this.className += ' hovered';
-}
+    }
 
-function dragLeave() {
-  this.className = 'restaurant-empty';
-}
+  }).catch(() => {
+      console.log('failed');
+    });
 
-function dragDrop() {
-  if (this === document.getElementById("restaurant-incoming")) {
-    console.log("drop-1");
-  } else if (this === document.getElementById("restaurant-in-progress")) {
-    console.log("drop-2");
-  } else if (this === document.getElementById("restaurant-complete")) {
-    $(".restaurant-time-started").text(moment());
-    $(".restaurant-time-status").text("Time Complete");
-  }
-  this.className = "restaurant-empty";
-  this.append(fill);
-}}).catch(() => {
-  console.log('failed');
-});
 });
 
