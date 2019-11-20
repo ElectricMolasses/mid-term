c0b52f58440b6328458c83050df8af1ca8166545
Robert Beckett <robert.beckett@onion.red>
2019-11-20 23:06:30 +0000

    Continued debugging on disappearing form.

--- public/scripts/user/user-menu.js
+++ public/scripts/user/user-menu.js
@@ -38,7 +38,7 @@ $(() => {
         if (!category.includes($category)) {
           category.push($category);
           
-          $temp.find(".user-menu-category"). text($category);
+          $temp.find(".user-menu-category").text($category);
           $temp.find(".user-item-name").text(data[index].name);
           $temp.find(".user-item-description").text(data[index].description);
           $temp.find(".user-item-price").text((Number(data[index].cost) / 100).toFixed(2));
