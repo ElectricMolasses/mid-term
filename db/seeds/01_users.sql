13a36d4a130b701faff87546885c9e6ff026e362
Robert Beckett <robert.beckett@onion.red>
2019-11-21 21:46:33 +0000

    Tied orders with no userId to a guest account.

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,6 +1,8 @@
 INSERT INTO users (
   user_token, first_name, last_name, email, password, phone_number
 ) VALUES (
+  'SJDGLJ#*@(RJ@#(JRLA', 'Guest', 'User', 'noreply@guest.com', 'IASJFO(QIJWDQ@)', '+19991119999'
+), (
   'ALSKVNASDJO@IJE)(!@', 'test', 'user', 'testUser@test.test', 'password', '+19023945393'
 ), (
   'A:SVNOISFJAOWJAWFIO', 'Owner', 'Mcgee', 'businessOwner@buss.ness', 'password', '+19023945393'
